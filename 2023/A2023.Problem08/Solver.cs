1fac30c86abbcbce7f252f8f3acf5b8fc267c3af
xiety <xiety666@gmail.com>
2023-12-08 12:24:58 +0200

    A2023.Problem08 B

--- 2023/A2023.Problem08/Solver.cs
+++ 2023/A2023.Problem08/Solver.cs
@@ -8,22 +8,65 @@ public class Solver : IProblemSolver<long>
 {
     public long RunA(string filename)
     {
-        var lines = File.ReadAllLines(filename);
-        var path = lines[0].Select(a => a switch { 'L' => 0, 'R' => 1 }).ToArray();
-        var nodes = CompiledRegs.Regex().FromLines<Node>(lines[2..]);
+        var (path, nodes) = LoadFile(filename);
 
         var step = 0;
-        var current_node = "AAA";
+        var current_node = nodes.First(a => a.Name == "AAA");
 
         do
         {
-            var node = nodes.First(a => a.Name == current_node);
-            current_node = node.Outputs[path[step % path.Length]];
+            var path_index = step % path.Length;
+            current_node = nodes.First(a => a.Name == current_node.Outputs[path[path_index]]);
             step++;
         }
-        while (current_node != "ZZZ");
+        while (current_node.Name != "ZZZ");
 
         return step;
+    }
+
+    public long RunB(string filename)
+    {
+        var (path, nodes) = LoadFile(filename);
+
+        var current_nodes = nodes.Where(a => a.Name.EndsWith('A')).ToArray();
+        var all = new List<List<int>>();
+
+        foreach (var start in current_nodes)
+        {
+            var step = 0;
+            var current_node = start;
+            var list = new List<int>();
+            var already = new List<(Node, int)>();
+
+            do
+            {
+                var path_index = step % path.Length;
+
+                if (already.Contains((current_node, path_index)))
+                    break;
+
+                already.Add((current_node, path_index));
+
+                current_node = nodes.First(b => b.Name == current_node.Outputs[path[path_index]]);
+                step++;
+
+                if (current_node.Name.EndsWith('Z'))
+                    list.Add(step);
+            }
+            while (true);
+
+            all.Add(list);
+        }
+
+        return MathExtensions.Lcm(all.Select(a => a.Max())); //little cheat with max
+    }
+
+    private static (int[] path, List<Node>) LoadFile(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        var path = lines[0].Select(a => a switch { 'L' => 0, 'R' => 1 }).ToArray();
+        var nodes = CompiledRegs.Regex().FromLines<Node>(lines[2..]);
+        return (path, nodes);
     }
 }
 
---
+++ Advent.Common/MathExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Advent.Common;
+
+public static class MathExtensions
+{
+    public static long Lcm(IEnumerable<int> values)
+    {
+        var primes = values.Select(Primes);
+        var list = new List<int>();
+
+        foreach (var ps in primes)
+            list.AddRange(ps.Where(a => !list.Contains(a)));
+
+        return list.MulLong();
+    }
+
+    public static IEnumerable<int> Primes(int value)
+    {
+        var current = value;
+
+        do
+        {
+            for (var i = 2; i <= current; ++i)
+            {
+                if ((current % i) == 0)
+                {
+                    yield return i;
+                    current /= i;
+                }
+            }
+        }
+        while (current > 1);
+    }
+}
