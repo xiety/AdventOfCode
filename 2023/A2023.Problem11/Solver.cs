0c1e14408dd92bba7ba09b18fab6e3132ced6c3c
xiety <xiety666@gmail.com>
2023-12-11 13:37:37 +0200

    A2023.Problem11 B

--- 2023/A2023.Problem11/Solver.cs
+++ 2023/A2023.Problem11/Solver.cs
@@ -1,18 +1,23 @@
-﻿using System.Reflection.Metadata;
-using System.Xml;
-
-using Advent.Common;
+﻿using Advent.Common;
 
 namespace A2023.Problem11;
 
 public class Solver : IProblemSolver<long>
 {
     public long RunA(string filename)
+        => Run(filename, 2);
+
+    public long RunB(string filename)
+        => Run(filename, Path.GetFileName(filename) == "sample.txt" ? 100 : 1_000_000);
+
+    private long Run(string filename, int resize)
     {
         var map = MapData.ParseMap(File.ReadAllLines(filename), c => c == '#');
 
-        var expanded = Expand(map);
-        var stars = expanded.EnumeratePositionsOf(-1).ToArray();
+        var emptyCols = GetEmptyCols(map);
+        var emptyRows = GetEmptyRows(map);
+
+        var stars = GetStars(map, resize, emptyCols, emptyRows).ToArray();
         var totalDistance = 0L;
 
         for (var i = 0; i < stars.Length - 1; ++i)
@@ -22,27 +27,20 @@ public class Solver : IProblemSolver<long>
         return totalDistance;
     }
 
-    private int[,] Expand(bool[,] map)
+    private IEnumerable<Pos> GetStars(bool[,] map, int resize, List<int> emptyCols, List<int> emptyRows)
     {
-        var emptyCols = new List<int>();
-        var emptyRows = new List<int>();
-
-        for (var x = 0; x < map.GetWidth(); ++x)
+        foreach (var pos in map.EnumeratePositionsOf(true))
         {
-            var empty = true;
+            var dx = emptyCols.Count(a => a < pos.X) * (resize - 1) + pos.X;
+            var dy = emptyRows.Count(a => a < pos.Y) * (resize - 1) + pos.Y;
 
-            for (var y = 0; y < map.GetHeight(); ++y)
-            {
-                if (map[x, y])
-                {
-                    empty = false;
-                    break;
-                }
-            }
-
-            if (empty)
-                emptyCols.Add(x);
+            yield return new(dx, dy);
         }
+    }
+
+    private static List<int> GetEmptyRows(bool[,] map)
+    {
+        var emptyRows = new List<int>();
 
         for (var y = 0; y < map.GetHeight(); ++y)
         {
@@ -61,32 +59,31 @@ public class Solver : IProblemSolver<long>
                 emptyRows.Add(y);
         }
 
-        var expanded = ArrayEx
-            .CreateAndInitialize(map.GetWidth() + emptyCols.Count, map.GetHeight() + emptyRows.Count, 1);
+        return emptyRows;
+    }
 
-        var ey = 0;
+    private static List<int> GetEmptyCols(bool[,] map)
+    {
+        var emptyCols = new List<int>();
 
-        for (var y = 0; y < map.GetHeight(); ++y)
+        for (var x = 0; x < map.GetWidth(); ++x)
         {
-            var ex = 0;
+            var empty = true;
 
-            for (var x = 0; x < map.GetWidth(); ++x)
+            for (var y = 0; y < map.GetHeight(); ++y)
             {
-                expanded[ex, ey] = map[x, y] ? -1 : 1;
-
-                if (emptyCols.Contains(x))
-                    ex++;
-
-                ex++;
+                if (map[x, y])
+                {
+                    empty = false;
+                    break;
+                }
             }
 
-            if (emptyRows.Contains(y))
-                ey++;
-
-            ey++;
+            if (empty)
+                emptyCols.Add(x);
         }
 
-        return expanded;
+        return emptyCols;
     }
 
     private static long Distance(Pos from, Pos to)
