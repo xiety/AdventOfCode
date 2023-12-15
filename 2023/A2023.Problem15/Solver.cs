2fd4b7d3fc1a619b0e1b140029287a462ea210d9
xiety <xiety666@gmail.com>
2023-12-15 13:10:28 +0200

    A2023.Problem15 A

---
+++ 2023/A2023.Problem15/Solver.cs
@@ -0,0 +1,29 @@
+﻿using Advent.Common;
+
+namespace A2023.Problem15;
+
+public class Solver : IProblemSolver<long>
+{
+    public long RunA(string filename)
+    {
+        var items = File.ReadAllText(filename).TrimEnd().Split(",");
+
+        return items.Sum(Hash);
+    }
+
+    private long Hash(string text)
+    {
+        var ret = 0;
+
+        var bytes = System.Text.Encoding.ASCII.GetBytes(text);
+
+        foreach (var b in bytes)
+        {
+            ret += b;
+            ret *= 17;
+            ret %= 256;
+        }
+
+        return ret;
+    }
+}
