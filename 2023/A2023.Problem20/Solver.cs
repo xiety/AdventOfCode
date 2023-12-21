2f77019136ce6ccf39779d14ef1d84725f150f01
xiety <xiety666@gmail.com>
2023-12-21 11:55:49 +0200

    A2023.Problem20 B

--- 2023/A2023.Problem20/Solver.cs
+++ 2023/A2023.Problem20/Solver.cs
@@ -1,5 +1,4 @@
-﻿using System.Data;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 using Advent.Common;
 
@@ -12,73 +11,165 @@ public class Solver : IProblemSolver<long>
     public long RunA(string filename)
     {
         var items = CompiledRegs.Regex().FromFile<Item>(filename);
-
         var graph = Create(items);
-
         var total = 1000;
 
         var dic = new Dictionary<bool, long>() { [false] = 0, [true] = 0 };
+        var conjDic = new Dictionary<string, List<long>>();
 
-        for (var i = 0; i < total; ++i)
+        var button = graph.OfType<RadioButton>().First();
+
+        var currentList = new List<Radio>();
+        var newList = new List<Radio>();
+
+        for (var i = 0L; i < total; ++i)
         {
-            var currentList = graph.OfType<RadioButton>().ToList<Radio>();
-            var newList = new List<Radio>();
-
-            do
-            {
-                foreach (var radio in currentList)
-                {
-                    if (radio is RadioButton button)
-                    {
-                        SendSignal(dic, newList, radio, false);
-                    }
-                    else if (radio is RadioBroadcaster broadcaster)
-                    {
-                        SendSignal(dic, newList, radio, false);
-                    }
-                    else if (radio is RadioFlipflop flipflop)
-                    {
-                        var (connection, pulse) = radio.PulseQueue.Dequeue();
-
-                        connection.PulseMemory = pulse;
-
-                        if (pulse == false)
-                        {
-                            flipflop.Turn = !flipflop.Turn;
-
-                            SendSignal(dic, newList, radio, flipflop.Turn ? true : false);
-                        }
-                    }
-                    else if (radio is RadioConjunction conjunction)
-                    {
-                        var (connection, pulse) = radio.PulseQueue.Dequeue();
-
-                        connection.PulseMemory = pulse;
-
-                        var allHigh = conjunction.Inputs.All(a => a.PulseMemory == true);
-
-                        SendSignal(dic, newList, radio, !allHigh);
-                    }
-                }
-
-                (currentList, newList) = (newList, currentList);
-                newList.Clear();
-            }
-            while (currentList.Count > 0);
+            currentList.Add(button);
+            Process(currentList, newList, dic, conjDic, false, i + 1L);
         }
 
         return dic.Select(a => a.Value).Mul();
     }
 
-    private static void SendSignal(Dictionary<bool, long> dic, List<Radio> newList, Radio radio, bool pulse)
+    public long RunB(string filename)
     {
-        foreach (var connection in radio.Outputs)
+        var items = CompiledRegs.Regex().FromFile<Item>(filename);
+        var graph = Create(items);
+
+        var deadend = graph.OfType<RadioDeadend>().First();
+
+        var dic = new Dictionary<bool, long>() { [false] = 0, [true] = 0 };
+
+        var buttonPress = 0L;
+
+        var button = graph.OfType<RadioButton>().First();
+
+        var currentList = new List<Radio>();
+        var newList = new List<Radio>();
+
+        var conjDic = new Dictionary<string, List<long>>();
+
+        var prev = FindConj(deadend).Inputs.Select(a => a.From.Name).ToList();
+
+        do
         {
+            currentList.Add(button);
+
+            buttonPress++;
+
+            if (Process(currentList, newList, dic, conjDic, true, buttonPress))
+                break;
+
+            if (conjDic.Where(a => prev.Contains(a.Key)).Count() == prev.Count
+             && conjDic.Where(a => prev.Contains(a.Key)).All(a => a.Value.Count > 1))
+                break;
+        }
+        while (true);
+
+        return MathExtensions.Lcm(conjDic.Where(a => prev.Contains(a.Key))
+            .Select(a => (int)(a.Value[1] - a.Value[0])));
+    }
+
+    private RadioConjunction FindConj(Radio parent)
+    {
+        var child = parent.Inputs.Select(a => a.From).OfType<RadioConjunction>().FirstOrDefault();
+
+        if (child is not null)
+            return child;
+
+        return parent.Inputs.Select(a => a.From).Select(FindConj).First();
+    }
+
+    private bool Process(List<Radio> currentList, List<Radio> newList, Dictionary<bool, long> dic, Dictionary<string, List<long>> conjDic, bool exit, long buttonPress)
+    {
+        var step = 1L;
+
+        do
+        {
+            foreach (var radio in currentList)
+            {
+                if (radio is RadioButton button)
+                {
+                    SendSignal(dic, newList, radio, false, exit);
+                }
+                else if (radio is RadioBroadcaster broadcaster)
+                {
+                    SendSignal(dic, newList, radio, false, exit);
+                }
+                else if (radio is RadioFlipflop flipflop)
+                {
+                    Flipflop(newList, dic, flipflop, exit);
+                }
+                else if (radio is RadioConjunction conjunction)
+                {
+                    Conjunction(newList, dic, conjunction, conjDic, exit, buttonPress, step);
+                }
+                else if (radio is RadioDeadend deadend)
+                {
+                    var (connection, pulse) = radio.PulseQueue.Dequeue();
+
+                    if (pulse == false && exit)
+                        return true;
+                }
+
+                step++;
+            }
+
+            (currentList, newList) = (newList, currentList);
+            newList.Clear();
+        }
+        while (currentList.Count > 0);
+
+        return false;
+    }
+
+    private static void Flipflop(List<Radio> newList, Dictionary<bool, long> dic, RadioFlipflop flipflop, bool exit)
+    {
+        var (connection, pulse) = flipflop.PulseQueue.Dequeue();
+
+        connection.PulseMemory = pulse;
+
+        if (pulse == false)
+        {
+            flipflop.Turn = !flipflop.Turn;
+
+            SendSignal(dic, newList, flipflop, flipflop.Turn, exit);
+        }
+    }
+
+    private static readonly Predicate<Connection> check1 = a => a.PulseMemory == true;
+
+    private void Conjunction(List<Radio> newList, Dictionary<bool, long> dic, RadioConjunction conjunction, Dictionary<string, List<long>> conjDic, bool exit, long buttonPress, long step)
+    {
+        var (connection, pulse) = conjunction.PulseQueue.Dequeue();
+
+        connection.PulseMemory = pulse;
+
+        var allHigh = Array.TrueForAll(conjunction.Inputs, check1);
+        var nextPulse = !allHigh;
+
+        if (nextPulse)
+        {
+            var list = conjDic.GetOrCreate(connection.To.Name, () => []);
+            list.Add(buttonPress);
+        }
+
+        SendSignal(dic, newList, conjunction, nextPulse, exit);
+    }
+
+    private static void SendSignal(Dictionary<bool, long> dic, List<Radio> newList, Radio radio, bool pulse, bool exit)
+    {
+        var size = radio.Outputs.Length;
+
+        for (var i = 0; i < size; ++i)
+        {
+            var connection = radio.Outputs[i];
             connection.To.PulseQueue.Enqueue((connection, pulse));
 
             newList.Add(connection.To);
 
-            dic[pulse]++;
+            if (!exit)
+                dic[pulse]++;
         }
     }
 
@@ -115,7 +206,7 @@ public class Solver : IProblemSolver<long>
             if (fromName == broadcasterName)
             {
                 var connection = new Connection { From = button, To = fromRadio };
-                button.Outputs.Add(connection);
+                button.Outputs = [.. button.Outputs, connection];
                 fromRadio.PulseQueue.Enqueue((connection, false));
             }
 
@@ -131,8 +222,8 @@ public class Solver : IProblemSolver<long>
 
                 var connection = new Connection { From = fromRadio, To = toRadio };
 
-                fromRadio.Outputs.Add(connection);
-                toRadio.Inputs.Add(connection);
+                fromRadio.Outputs = [.. fromRadio.Outputs, connection];
+                toRadio.Inputs = [.. toRadio.Inputs, connection];
             }
         }
 
@@ -156,12 +247,12 @@ public record Item(string Name, string[] Outputs);
 
 public abstract class Radio
 {
-    public required string Name { get; init; }
+    public required string Name;
 
-    public List<Connection> Inputs { get; } = [];
-    public List<Connection> Outputs { get; } = [];
+    public Connection[] Inputs = [];
+    public Connection[] Outputs = [];
 
-    public Queue<(Connection, bool)> PulseQueue { get; } = [];
+    public Queue<(Connection, bool)> PulseQueue = [];
 }
 
 public class RadioButton : Radio
@@ -178,7 +269,7 @@ public class RadioDeadend : Radio
 
 public class RadioFlipflop : Radio
 {
-    public bool Turn { get; set; }
+    public bool Turn;
 }
 
 public class RadioConjunction : Radio
@@ -187,10 +278,10 @@ public class RadioConjunction : Radio
 
 public class Connection
 {
-    public required Radio From { get; set; }
-    public required Radio To { get; set; }
+    public required Radio From;
+    public required Radio To;
 
-    public bool PulseMemory { get; set; }
+    public bool PulseMemory;
 }
 
 static partial class CompiledRegs
