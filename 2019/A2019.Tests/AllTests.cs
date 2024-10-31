9ad14ec57b0c50e352f338d74fd724d2a1178c60
xiety <xiety666@gmail.com>
2024-10-31 18:28:05 +0200

    A2019.Problem04

--- 2019/A2019.Tests/AllTests.cs
+++ 2019/A2019.Tests/AllTests.cs
@@ -8,14 +8,14 @@ namespace A2019.Tests;
 public class AllTests : BaseProblemTest
 {
     [ProblemData<int>(33583 + 654 + 2 + 2, 3311492, 50346 + 966 + 2, 4964376)]
-    public void Problem01(object p)
-        => Test(new Problem01.Solver(), p);
+    public void Problem01(object p) => Test(new Problem01.Solver(), p);
 
     [ProblemData<int>(3500, 3058646, 0, 8976)]
-    public void Problem02(object p)
-        => Test(new Problem02.Solver(), p);
+    public void Problem02(object p) => Test(new Problem02.Solver(), p);
 
     [ProblemData<int>(135, 627, 410, 13190)]
-    public void Problem03(object p)
-        => Test(new Problem03.Solver(), p);
+    public void Problem03(object p) => Test(new Problem03.Solver(), p);
+
+    [ProblemData<int>(64, 960, 51, 626)]
+    public void Problem04(object p) => Test(new Problem04.Solver(), p);
 }
