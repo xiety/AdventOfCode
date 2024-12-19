d5b6a1e904fe804a1de40c8d75310edc06a1b4c6
xiety <xiety666@gmail.com>
2024-12-19 21:47:44 +0200

    A2024.Problem19 (dp)

--- 2024/A2024.Problem19/Solver.cs
+++ 2024/A2024.Problem19/Solver.cs
@@ -37,6 +37,20 @@ public class Solver : ISolver<long>
             .Sum();
     }
 
+    //https://www.reddit.com/r/adventofcode/comments/1hhvroq/2024_day_19_python_dynamic_programing_solution/
+    static long CountDP(ReadOnlySpan<char> goal, string[] patterns)
+    {
+        var dp = new long[goal.Length + 1];
+        dp[0] = 1;
+
+        for (var i = 1; i < dp.Length; ++i)
+            foreach (var pattern in patterns)
+                if (pattern.Length <= i && goal[(i - pattern.Length)..i].SequenceEqual(pattern))
+                    dp[i] += dp[i - pattern.Length];
+
+        return dp[^1];
+    }
+
     static (string[], string[]) LoadData(string[] lines)
     {
         var towels = lines[0].Split(", ").ToArray();
