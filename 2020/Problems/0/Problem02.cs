0c1840c99c18a69ce1ea8ef0e5503351a09d037c
xiety <xiety666@gmail.com>
2025-11-13 12:45:56 +0200

    A2020.Problem02

---
+++ 2020/Problems/0/Problem02.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+using Advent.Common;
+
+namespace A2020.Problem02;
+
+public class Solver : ISolver<int>
+{
+    public int RunA(string[] lines, bool isSample)
+        => Run(lines, CheckA);
+
+    public int RunB(string[] lines, bool isSample)
+        => Run(lines, CheckB);
+
+    static int Run(string[] lines, Func<Item, bool> predicate)
+        => LoadData(lines).Where(predicate).Count();
+
+    static bool CheckA(Item item)
+    {
+        var num = item.Text.Count(a => a == item.Letter);
+        return num >= item.From && num <= item.To;
+    }
+
+    static bool CheckB(Item a)
+    {
+        return Check(a.From - 1) ^ Check(a.To - 1);
+        bool Check(int pos) => pos < a.Text.Length && a.Text[pos] == a.Letter;
+    }
+
+    static Item[] LoadData(string[] lines)
+        => CompiledRegs.Regex().FromLines<Item>(lines);
+}
+
+record Item(int From, int To, char Letter, string Text);
+
+static partial class CompiledRegs
+{
+    [GeneratedRegex(@$"^(?<{nameof(Item.From)}>\d+)-(?<{nameof(Item.To)}>\d+)\s(?<{nameof(Item.Letter)}>.)\:\s(?<{nameof(Item.Text)}>.+)$")]
+    public static partial Regex Regex();
+}
--- 2020/Tests/AllTests.cs
+++ 2020/Tests/AllTests.cs
@@ -7,4 +7,7 @@ public class AllTests : FolderSolverTest
 {
     [ProblemData<int>(514579, 997899, 241861950, 131248694)]
     public void Problem01(ITestParameter p) => Test(new Problem01.Solver(), p);
+
+    [ProblemData<int>(2, 638, 1, 699)]
+    public void Problem02(ITestParameter p) => Test(new Problem02.Solver(), p);
 }
