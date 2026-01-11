b19b7f4e3cad64eb5a91891a226a05bff4d3b06b
xiety <xiety666@gmail.com>
2026-01-11 12:29:26 +0200

    A2015.Problem04

---
+++ 2015/0/Problem04/Problem04.cs
@@ -0,0 +1,42 @@
+﻿#pragma warning disable CA5351 // Do Not Use Broken Cryptographic Algorithms
+
+using System.Text;
+
+namespace A2015.Problem04;
+
+public static class Solver
+{
+    [GeneratedTest<int>(609043 + 1048970, 117946)]
+    public static int RunA(string[] lines)
+        => lines.Sum(a => Calc(a, "00000"));
+
+    [GeneratedTest<int>(-1, 3938038)]
+    public static int RunB(string[] lines, bool isSample)
+        => isSample ? -1 : lines.Sum(a => Calc(a, "000000"));
+
+    static int Calc(string text, string start)
+    {
+        var n = 1;
+
+        do
+        {
+            var input = text + n.ToString();
+            var h = CalcMD5(input);
+
+            if (h.StartsWith(start))
+                break;
+
+            n++;
+        }
+        while (true);
+
+        return n;
+    }
+
+    static string CalcMD5(string input)
+    {
+        var inputBytes = Encoding.UTF8.GetBytes(input);
+        var hashBytes = System.Security.Cryptography.MD5.HashData(inputBytes);
+        return String.Concat(hashBytes.Select(a => a.ToString("x2")));
+    }
+}
